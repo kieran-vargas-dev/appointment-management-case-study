f07cd02c02583c94c254951c1354037b73e57ac6
Kieran Vargas <kieran.vargas.fullstack@gmail.com>
2022-03-07 11:56:55 -0700

    Initialized local repository

---
+++ src/main/java/com/kieran/appointment_management_tool/scheduler_capstone/SchedulerCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.kieran.appointment_management_tool.scheduler_capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulerCapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kieran/appointment_management_tool/scheduler_capstone/SchedulerCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kieran.appointment_management_tool.scheduler_capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
